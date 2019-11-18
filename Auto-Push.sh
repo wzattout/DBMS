9d83f5c660260ab1aaa68c19d27d7dac2b14683d
Walid Zattout <wzattout@gmail.com>
2019-11-18 17:26:37 +0200

    auto-push

---
+++ Auto-Push.sh
@@ -0,0 +1,8 @@
+cd "C:\Users\wzatt\Documents\Programming\JAVA\CSED\Y2_T1\Programming\Labs\3\oop-dbms";
+git submodule foreach 'git checkout walid';
+git submodule foreach 'git add .';
+git submodule foreach 'git commit -m "walid habad" | true';
+git submodule foreach 'git push';
+git add .;
+git commit -m "auto-push";
+git push;
---
+++ Update-Submodules.sh
@@ -0,0 +1,16 @@
+cd "C:\Users\wzatt\Documents\Programming\JAVA\CSED\Y2_T1\Programming\Labs\3\oop-dbms\src\eg\edu\alexu\csd\oop";
+
+git checkout submodule;
+git pull;
+
+git checkout mostafa;
+git pull;
+
+git checkout marwan;
+git pull;
+
+git checkout ahmed;
+git pull;
+
+
+git checkout walid;
