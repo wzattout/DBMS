2d0ed543fd632748c3976da5afdba626bbbef6e6
Walid Zattout <wzattout@gmail.com>
2020-03-09 17:45:18 +0200

    Last commit, jdbc updated

---
+++ bin/eg/edu/alexu/csd/oop/UpdateSubmodule.sh
@@ -0,0 +1,3 @@
+cd "C:\Users\wzatt\Documents\Programming\JAVA\CSED\Y2_T1\Programming\Labs\4\oop-jdbc\src\eg\edu\alexu\csd\oop\dbms";
+git pull -f origin jar4jdbc:jar4jdbc; 
+cmd /k command;
---
+++ bin/eg/edu/alexu/csd/oop/auto-push.sh
@@ -0,0 +1,5 @@
+cd "C:\Users\wzatt\Documents\Programming\JAVA\CSED\Y2_T1\Programming\Labs\4\oop-jdbc\src\eg\edu\alexu\csd\oop";
+git add .;
+git commit;
+git push; 
+cmd /k command;
