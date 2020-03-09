5026db6aeab60c9f521cd8476a6eea95309a2e12
Walid Zattout <wzattout@gmail.com>
2020-03-09 17:37:19 +0200

    Last commit, dbms updated

---
+++ Launcher.bat
@@ -0,0 +1,4 @@
+@echo off
+java -jar "C:/Users/wzatt/Documents/Programming/JAVA/CSED/Y2_T1/Programming/Labs/3/oop-dbms/out/production/oop-dbms/eg/edu/alexu/csd/oop/db/cs70/Main.jar"
+pause
+exit
--- Update-Submodules.sh
+++ Update-Submodules.sh
@@ -1,6 +1,6 @@
 cd "C:\Users\wzatt\Documents\Programming\JAVA\CSED\Y2_T1\Programming\Labs\3\oop-dbms\src\eg\edu\alexu\csd\oop";
 
-git checkout submodule;
+git checkout master;
 git pull;
 
 git checkout walid;
